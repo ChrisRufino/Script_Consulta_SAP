0f4ef416ba7d719da589d4d55b2b7b9b0bcce53a
Christian Rufino <132108817+ChrisRufino@users.noreply.github.com>
2026-01-26 21:07:28 -0300

    SetColorDateas

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import QApplication, QMainWindow,QGraphicsDropShadowEffect
+from PySide6.QtGui import QColor
 import sys
 from ui_mai import Ui_MainWindow
+
+
+show_elements = {
+    'frame','frame_8','frame_9','frame_logo'
+}
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setupUi(self)
+        self.txt_period_final.setInputMask('**.**.****')
+        self.txt_periodo_inicial.setInputMask('**.**.****')
 
+        for x in show_elements:
+            effect = QGraphicsDropShadowEffect(self)
+            effect.setBlurRadius(18)
+            effect.setXOffset(0)
+            effect.setYOffset(0)
+            effect.setColor(QColor(0,0,0,255))
+            getattr(self,x).setGraphicsEffect(effect)
 
 
 if __name__ == "__main__":
